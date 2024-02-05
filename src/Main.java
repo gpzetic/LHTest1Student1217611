55bc08b550f27d37d1ace32441297471a6f3ee65
gz <gz@gpmz.ca>
2024-02-05 13:43:54 -0500

    Question 3.4 complete

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@ public class Main {
 
         Component c = new Component("ggg", "XTREEM", 9999.993d);
         System.out.println(c.toString());
-
+        ShoppingCart sc = new ShoppingCart();
+        sc.addItem(c);
+        sc.addItem(c);
+        sc.addItem(c);
+        System.out.println(sc.getComponentNames());
+        System.out.println(sc.getTotalPrice());
     }
 }
--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -4,5 +4,22 @@ public class ShoppingCart {
     public ShoppingCart() {
         components = new ArrayList<>();
     }
-
+    public void addItem(Component c) {
+        components.add(c);
+    }
+    public double getTotalPrice() {
+        double sum = 0.0d;
+        for (Component c : components) {
+            sum += c.getPrice();
+        }
+        return sum;
+    }
+    public String getComponentNames() {
+        String componentNames = "";
+        for (int i = 0; i < components.size(); i ++) {
+            componentNames += components.get(i).getName();
+            if (i + 1 != components.size()) componentNames += ", ";
+        }
+        return componentNames;
+    }
 }
